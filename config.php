16be99c09820157a16eed80337a3047355e34d1c
sektor12 <nemanja.paradjina@gmail.com>
2019-04-14 00:41:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'nrt4007');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'test');
 
 $config['mainBackendTheme'] = 'adminSimpleGreen';
